725d676857336af8d26ce4a6bdcbf6a38b0958ba
Sarah Hinton <smcfarland2@gmail.com>
2021-07-10 15:56:48 +1000

    adding get and put routes to update read and update workouts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,6 +4,7 @@ const router = require('express').Router();
 const dbmodels = require('../models');
 const { db } = require('../models/exercise');
 
+// CREATE a workout
 router.post('/api/workouts', ({ body }, res) => {
     dbmodels.Workout.create(body)
     .then(dbworkoutTracking => {
@@ -13,3 +14,39 @@ router.post('/api/workouts', ({ body }, res) => {
         res.status(400).json(err);
     });
 });
+
+// READ workout
+router.get('/api/workouts/range', (req, res) => {
+    dbmodels.Workout.find({})
+    .then(dbworkoutTracking => {
+        res.json(dbworkoutTracking);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+})
+
+// UPDATE workout
+router.put('/api/workouts/:id', ({ params }, res) => {
+    dbmodels.Workout.findByIdAndUpdate(
+        {
+            _id: req.params.id
+        },
+        {
+            $addToSet: {
+                exercise: req.body
+            }
+        },
+        {
+            new: true
+        }
+    )
+    .then((dbworkoutTracking) => {
+        res.json(dbworkoutTracking);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+
