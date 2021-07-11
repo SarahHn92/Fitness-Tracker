a0378e0fc52ef90308692def67217c7c6ba33e0d
Sarah Hinton <smcfarland2@gmail.com>
2021-07-11 17:37:31 +1000

    added /index to routes path

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require('morgan');
-const routes = require('./routes');
+const routes = require('./routes/index');
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
